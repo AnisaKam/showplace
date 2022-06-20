5a81a2ad8ce38aab8b3ed6fb7f6d05df4de6add3
AnisaKamilianova <anisa_murtazina@yahoo.com>
2022-06-20 20:33:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const[place, setPlace] = useState(0);
+  const {id, showplace, description, image} = data[place];
+
+  const previousPlace = () => {
+    setPlace((place => {
+      place --;
+      if (place < 0) {
+        return data.length - 1
+      }
+      return place;
+    }))
+  }
+
+  const nextPlace = () => {
+    setPlace((place => {
+      place ++;
+      if (place > data.length - 1) {
+        place = 0;
+      }
+      return place;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+
+    <div className='container fonts'>
+      <h1>{id} - {showplace}</h1>
     </div>
-  );
+
+    <div className='container fonts'>
+      <img src={image} width='370px' alt='showplace'/>
+    </div>
+
+    <div className='container fonts'>
+      <h2>{description}</h2>
+    </div>
+
+    <div className='btn container fonts'>
+      <button onClick={previousPlace}>Previous</button>
+      <button onClick={nextPlace}>Next</button>
+    </div>
+
+
+
+</div>
+  )
 }
 
 export default App;
